8096fa7f32105ad7ea7e7957d7fc3afabecb30c7
ausias <ausias@h7-43-THINK-2020-21>
2022-01-12 09:43:33 +0100

    1r modificacio

--- Practica6/src/practica6/Practica6.java
+++ Practica6/src/practica6/Practica6.java
@@ -14,7 +14,7 @@ public class Practica6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola de part del net beans");
     }
     
 }
